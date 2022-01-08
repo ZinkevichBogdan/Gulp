27fea921afb6ffe8c9219808e95b033b87b96b87
ZinkevichBogdan <Zinkevicbogdan738@gmail.com>
2022-01-08 17:15:40 +0200

    My first Gulp

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+
+// Импорт пакетов
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const browsersync = require('browser-sync').create()
+
+// Пути исходных файлов src
+
+const paths = {
+
+   html: {
+      src: ['src/*.html'],
+      dest: 'dist'
+   },
+
+   scss: {
+      src: 'src/scss/**/*.scss',
+      dest: 'dist/css/'
+   },
+   scripts: {
+      src: 'src/scripts/**/*.js',
+      dest: 'dist/js/'
+   },
+   images: {
+      src: 'src/img/**',
+      dest: 'dist/img/'
+   }
+}
+
+// Очистить каталог dist, удалить все кроме изображений
+function clean() {
+   return del(['dist/*', '!dist/img'])
+}
+
+// Обработка htm
+function html() {
+   return gulp.src(paths.html.src)
+      .pipe(gulp.dest(paths.html.dest))
+      .pipe(browsersync.stream())
+}
+
+// Обработка препроцессоров стилей
+function scss() {
+   return gulp.src(paths.scss.src)
+      .pipe(sass())
+      .pipe(gulp.dest(paths.scss.dest))
+      .pipe(browsersync.stream())
+}
+
+// Обработка изображений
+
+function img() {
+   return gulp.src(paths.images.src)
+      .pipe(gulp.dest(paths.images.dest))
+}
+
+
+
+
+function scripts() {
+   return gulp.src(paths.scripts.src,)
+      .pipe(gulp.dest(paths.scripts.dest))
+      .pipe(browsersync.stream())
+}
+
+// Отслеживание изменений и запуск  сервера
+function watch() {
+   browsersync.init({
+      server: {
+          baseDir: "./dist"
+      }
+    })
+   gulp.watch(paths.scss.src, scss)
+   gulp.watch(paths.scripts.src, scripts)
+}
+
+// Задача которая выполняется по команде gulp
+
+const build = gulp.series(clean, html, gulp.parallel(scripts, scss, img), watch)
+
+// Задача для ручного запуска
+
+exports.clean = clean
+exports.html = html
+exports.img = img
+exports.scss = scss
+exports.watch = watch
+exports.build = build
+exports.default = build
+exports.scripts = scripts
